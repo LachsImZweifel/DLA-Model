885a953e16489a197e979b388709a0c290dd9647
Jan Lattenkamp <jan_ralph.lattenkamp@smail.th-koeln.de>
2024-05-28 11:29:34 +0200

    Early Promodoro Break, because of move to s22

--- DlaModel.js
+++ DlaModel.js
@@ -28,3 +28,23 @@ class Particle {
         this.isCrystal = isCrystal;
     }
 }
+
+class Grid {
+    constructor(){
+        this.fields = fields[50][50];
+    }
+
+    initiateParticles(){
+        for (let i = 0; i < 250; i++) {
+            let xPos = random(0,50);
+            let yPos = random(0,50);
+            this.fields[xPos][yPos].push(new Particle(xPos, yPos));
+        }
+    }
+
+    updateParticles(){
+        this.fields.forEach(element => {
+            
+        });
+    }
+}
